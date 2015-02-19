afa1c0feca4d13916588c52227a413e6e50cae95
Andrei Lisnic <andrei.lisnic@gmail.com>
2015-02-19 12:53:17 +0200

    Rename the project some more, improve the comment

--- dotnet/ConsoleApplication1/Program.cs
+++ dotnet/ConsoleApplication1/Program.cs
@@ -10,17 +10,19 @@ namespace SESample
     {
         static void Main(string[] args)
         {
-            //System.Console.WriteLine(RestClient.generateSignature("GET", "foo.com"));
             System.Console.WriteLine(SaltEdge.get("https://www.saltedge.com/api/v2/countries"));
             
-            //var payload = new {
-            //    data = new {
-            //        identifier = "12rv1212f1efxchsdhbgv"
-            //    }
-            //};
-
-            //string result = RestClient.post("https://www.saltedge.com/api/v2/customers/", payload);
-            //System.Console.WriteLine(result);
+            // This is an example of a post request with a payload. Keep in mind the the serialized payload
+            // is also taken into account when computing the signature.
+            //
+            // var payload = new {
+            //     data = new {
+            //         identifier = "12rv1212f1efxchsdhbgv"
+            //     }
+            // };
+            //
+            // string result = RestClient.post("https://www.saltedge.com/api/v2/customers/", payload);
+            // System.Console.WriteLine(result);
             System.Console.ReadKey();
         }
     }
