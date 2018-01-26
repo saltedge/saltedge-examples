6a0f72e9bd9c859702856030c082ff6138a72a31
Andrei Lisnic <andrei.lisnic@gmail.com>
2018-01-26 16:03:01 +0200

    Update java example

--- java/java-saltedge-example/src/com/saltedge/Program.java
+++ java/java-saltedge-example/src/com/saltedge/Program.java
@@ -4,7 +4,7 @@ package com.saltedge;
 public class Program {
 
     public static void main (String[] arg) {
-    	System.out.println(new SaltEdge().get("https://www.saltedge.com/api/v3/countries"));
+    	System.out.println(new SaltEdge().get("https://www.saltedge.com/api/v4/countries"));
     	// This is an example of a post request with a payload. Keep in mind the the serialized payload
         // is also taken into account when computing the signature.
 //
@@ -13,6 +13,6 @@ public class Program {
 //        JsonElement jelement = new JsonParser().parse(str);
 //        JsonObject jobject = jelement.getAsJsonObject();
 //
-//        System.out.println(new SaltEdge().post("https://www.saltedge.com/api/v3/customers", jobject));
+//        System.out.println(new SaltEdge().post("https://www.saltedge.com/api/v4/customers", jobject));
     }
 }
