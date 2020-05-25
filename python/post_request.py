a0b68d558bbfba4d985dc0e051f93e6f6fe4cee1
Vlad Somov <vlad.somov93@gmail.com>
2020-05-25 12:08:26 +0300

    Removed extra argument from post put request that is not used

--- python/post_request.py
+++ python/post_request.py
@@ -1,9 +1,9 @@
 from saltedge import SaltEdge
 import json
 
-app = SaltEdge('client-id', 'service-secret', './private.pem', './public.pem')
+app = SaltEdge("client-id", "service-secret", "./private.pem")
 
-url = 'https://www.saltedge.com/api/v5/customers/'
+url = "https://www.saltedge.com/api/v5/customers/"
 payload = json.dumps({"data": {"identifier": "some_uniq_identifier"}})
 response = app.post(url, payload)
 data = response.json()
--- python/put_request.py
+++ python/put_request.py
@@ -1,9 +1,9 @@
 from saltedge import SaltEdge
 import json
 
-app = SaltEdge('client-id', 'service-secret', './private.pem', './public.pem')
+app = SaltEdge("client-id", "service-secret", "./private.pem")
 
-url = 'https://www.saltedge.com/api/v5/logins/1234/refresh'
+url = "https://www.saltedge.com/api/v5/logins/1234/refresh"
 payload = json.dumps({"data": {"fetch_type": "recent"}})
 response = app.put(url, payload)
 data = response.json()
