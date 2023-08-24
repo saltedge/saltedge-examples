9409db2e5a41e1155a4e0db1276b8c4271558cbd
Misha Dolghintev <41511298+mihaildolghintev@users.noreply.github.com>
2023-08-24 10:08:16 +0300

    rewrite dotnet examples (#92)
    * rewrite dotnet examples
    
    * updated URLS
    
    * delete key
    
    * updated readme. deleted keys
    
    * added configuration
    
    ---------
    
    Co-authored-by: mihail.dolghintev <mihail.dolghintev@saltedge.com>

---
+++ dotnet/requestsApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SESample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // *********************************
+            // ****** GET request example ******
+            // *********************************
+
+            System.Console.WriteLine(await SaltEdge.getAsync("https://www.saltedge.com/api/v5/countries"));
+
+
+
+            // **********************************
+            // ****** POST request example ******
+            // **********************************
+
+            // This is an example of a post request with a payload. Keep in mind the the serialized payload
+            // is also taken into account when computing the signature.
+            //
+            // var payload = new {
+            //     data = new {
+            //         identifier = "your_customer_email@domain.com"
+            //     }
+            // };
+
+            // string result = await SaltEdge.postAsync("https://www.saltedge.com/api/v5/customers/", payload);
+            // System.Console.WriteLine(result);
+            System.Console.ReadKey();
+        }
+    }
+}
