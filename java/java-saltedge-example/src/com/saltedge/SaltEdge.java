a522308dbb46491656ee472fc31a694ecc07aa29
Iuri Manolachi <iuri.manolachi@saltedge.com>
2023-09-06 17:33:15 +0300

    fixed-indent

--- java/java-saltedge-example/src/com/saltedge/SaltEdge.java
+++ java/java-saltedge-example/src/com/saltedge/SaltEdge.java
@@ -17,154 +17,154 @@ import java.util.Calendar;
 
 public class SaltEdge {
 
-	public final static int REQUEST_EXPIRES_MINUTES = 3;
-	public final static String APP_ID = "APP_ID";
-	public final static String SECRET = "SECRET";
-	public final String PRIVATE_KEY_PATH = "private.pem";
-	private static PEMKeyPair PRIVATE_KEY = null;
+  public final static int REQUEST_EXPIRES_MINUTES = 3;
+  public final static String APP_ID = "APP_ID";
+  public final static String SECRET = "SECRET";
+  public final String PRIVATE_KEY_PATH = "private.pem";
+  private static PEMKeyPair PRIVATE_KEY = null;
 
-	public SaltEdge() {
+  public SaltEdge() {
     PRIVATE_KEY = SignHelper.readPrivateKey(PRIVATE_KEY_PATH);
   }
 
-	// HTTP GET request
-	public String get(String url) {
-		HttpURLConnection con = buildRequest("GET", url);
+  // HTTP GET request
+  public String get(String url) {
+    HttpURLConnection con = buildRequest("GET", url);
 
-		if (con == null) {
-			return "";
-		}
+    if (con == null) {
+    return "";
+    }
 
-		// Add request header
-		long expires = generateExpiresAt();
-		con.setRequestProperty("Signature", generateSignature("GET", expires, url, ""));
-		con.setRequestProperty("Expires-at", String.valueOf(expires));
-		con.setRequestProperty("Content-Type", "application/json");
-		con.setRequestProperty("Accept", "application/json");
+    // Add request header
+      long expires = generateExpiresAt();
+      con.setRequestProperty("Signature", generateSignature("GET", expires, url, ""));
+      con.setRequestProperty("Expires-at", String.valueOf(expires));
+      con.setRequestProperty("Content-Type", "application/json");
+      con.setRequestProperty("Accept", "application/json");
 
-		return processResponse(con);
-	}
+      return processResponse(con);
+  }
 
-	// HTTP POST request
-	public String post(String url, Object payload) {
-		HttpURLConnection con = buildRequest("POST", url);
+  // HTTP POST request
+  public String post(String url, Object payload) {
+    HttpURLConnection con = buildRequest("POST", url);
 
-		if (con == null) {
-			return "";
-		}
+    if (con == null) {
+      return "";
+    }
 
-		// Add request header
-		long expires = generateExpiresAt();
-		con.setRequestProperty("Expires-at", String.valueOf(expires));
+    // Add request header
+    long expires = generateExpiresAt();
+    con.setRequestProperty("Expires-at", String.valueOf(expires));
 
-		// Generate json
-		Gson gson = new Gson();
-		String json = gson.toJson(payload);
+    // Generate json
+    Gson gson = new Gson();
+    String json = gson.toJson(payload);
 
-		con.setRequestProperty("Signature", generateSignature("POST", expires, url, json));
-		con.setRequestProperty("Content-Type", "application/json");
-		con.setRequestProperty("Accept", "application/json");
-		con.setDoOutput(true);
-		DataOutputStream wr;
+    con.setRequestProperty("Signature", generateSignature("POST", expires, url, json));
+    con.setRequestProperty("Content-Type", "application/json");
+    con.setRequestProperty("Accept", "application/json");
+    con.setDoOutput(true);
+    DataOutputStream wr;
 
-		try {
-			wr = new DataOutputStream(con.getOutputStream());
-			wr.writeBytes(json);
-			wr.flush();
-			wr.close();
-		} catch (IOException e) {
-			System.out.println("IOException : " + e);
-			e.printStackTrace();
-		}
+    try {
+      wr = new DataOutputStream(con.getOutputStream());
+      wr.writeBytes(json);
+      wr.flush();
+      wr.close();
+    } catch (IOException e) {
+      System.out.println("IOException : " + e);
+      e.printStackTrace();
+    }
 
-		return processResponse(con);
-	}
+    return processResponse(con);
+  }
 
-	private String processResponse(HttpURLConnection con) {
-		BufferedReader in;
-		StringBuffer response = new StringBuffer();
+  private String processResponse(HttpURLConnection con) {
+    BufferedReader in;
+    StringBuffer response = new StringBuffer();
 
-		try {
-			in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
+    try {
+      in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      String inputLine;
 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-		} catch (IOException e) {
-			System.out.println("IOException : " + e);
-			e.printStackTrace();
-		}
+      while ((inputLine = in.readLine()) != null) {
+        response.append(inputLine);
+      }
+      in.close();
+    } catch (IOException e) {
+      System.out.println("IOException : " + e);
+      e.printStackTrace();
+    }
 
-		return response.toString();
-	}
+    return response.toString();
+  }
 
-	private HttpURLConnection buildRequest(String method, String url) {
-		HttpURLConnection con = null;
+  private HttpURLConnection buildRequest(String method, String url) {
+    HttpURLConnection con = null;
 
-		try {
-			URL obj = new URL(url);
-			con = (HttpURLConnection) obj.openConnection();
-			con.setRequestMethod(method);
-			con.setRequestProperty("App-id", APP_ID);
-			con.setRequestProperty("Secret", SECRET);
-			con.setRequestProperty("Content-Type", "application/json");
-		} catch (ProtocolException e) {
-			System.out.println("ProtocolException : " + e);
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("IOException : " + e);
-			e.printStackTrace();
-		}
+    try {
+      URL obj = new URL(url);
+      con = (HttpURLConnection) obj.openConnection();
+      con.setRequestMethod(method);
+      con.setRequestProperty("App-id", APP_ID);
+      con.setRequestProperty("Secret", SECRET);
+      con.setRequestProperty("Content-Type", "application/json");
+    } catch (ProtocolException e) {
+      System.out.println("ProtocolException : " + e);
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.out.println("IOException : " + e);
+      e.printStackTrace();
+    }
 
     return con;
   }
 
-	private long generateExpiresAt() {
-		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.MINUTE, REQUEST_EXPIRES_MINUTES);
+  private long generateExpiresAt() {
+    Calendar calendar = Calendar.getInstance();
+    calendar.add(Calendar.MINUTE, REQUEST_EXPIRES_MINUTES);
 
-		return calendar.getTimeInMillis() / 1000;
+    return calendar.getTimeInMillis() / 1000;
   }
 
-	private String generateSignature(String method, long expires, String url, String postBody) {
-		String signature = String.format("%d|%s|%s|%s", expires, method, url, postBody);
-		byte[] bytes = signature.getBytes();
-		byte[] shaSignature = null;
+  private String generateSignature(String method, long expires, String url, String postBody) {
+    String signature = String.format("%d|%s|%s|%s", expires, method, url, postBody);
+    byte[] bytes = signature.getBytes();
+    byte[] shaSignature = null;
 
-		try {
-			shaSignature = sign(bytes);
-		} catch (SignatureException e) {
-			System.out.println("SignatureException : " + e);
-			e.printStackTrace();
-		} catch (PEMException e) {
-			System.out.println("PEMException : " + e);
-			e.printStackTrace();
-		}
+    try {
+      shaSignature = sign(bytes);
+    } catch (SignatureException e) {
+      System.out.println("SignatureException : " + e);
+      e.printStackTrace();
+    } catch (PEMException e) {
+      System.out.println("PEMException : " + e);
+      e.printStackTrace();
+    }
 
     return Base64.toBase64String(shaSignature);
   }
 
     private byte[] sign(byte[] bytes) throws SignatureException, PEMException {
-    	KeyPair keyPair=null;
-    	Signature signature = null;
+      KeyPair keyPair=null;
+      Signature signature = null;
 
-		try {
-			signature = Signature.getInstance("SHA256withRSA");
-			signature.initSign(new JcaPEMKeyConverter().getPrivateKey(PRIVATE_KEY.getPrivateKeyInfo()));
-			signature.update(bytes);
-		} catch (NoSuchAlgorithmException e) {
-			System.out.println("NoSuchAlgorithmException : " + e);
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SignatureException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    try {
+      signature = Signature.getInstance("SHA256withRSA");
+      signature.initSign(new JcaPEMKeyConverter().getPrivateKey(PRIVATE_KEY.getPrivateKeyInfo()));
+      signature.update(bytes);
+    } catch (NoSuchAlgorithmException e) {
+      System.out.println("NoSuchAlgorithmException : " + e);
+      e.printStackTrace();
+    } catch (InvalidKeyException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (SignatureException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
-  	return signature.sign();
+    return signature.sign();
   }
 }
